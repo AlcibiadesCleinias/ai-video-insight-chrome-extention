2e9c97599bc85b7e563c6477f57113576dcb28aa
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2024-07-04 18:51:42 +0200

    add youtube demo link

---
+++ chrome-extention/menuView/popup.js
@@ -0,0 +1,7 @@
+document.addEventListener('DOMContentLoaded', function() {
+  document.getElementById("watchDemoButton").addEventListener("click", handler);
+});
+
+function handler() {
+    window.open('https://youtu.be/CIYABA9aeCU','_blank')
+}
