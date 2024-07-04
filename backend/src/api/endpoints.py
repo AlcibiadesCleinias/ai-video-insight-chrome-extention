90509bded1b00f4a46d6d57b9e20c1f1dfac1c3f
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2024-07-04 15:29:52 +0200

    add transcript fetcher

--- backend/src/api/endpoints.py
+++ backend/src/api/endpoints.py
@@ -2,8 +2,13 @@ from fastapi import APIRouter
 
 import schemas
 
+from clients.youtube_scrapper_client import YoutubeScrapperClient
+from config.logger import get_app_logger
+
+logger = get_app_logger()
 router = APIRouter()
 
+# TODO: add caching {fetched data from youtube, ai insights responses}.
 @router.post(
     '/youtube',
     description=(
@@ -12,9 +17,17 @@ router = APIRouter()
     response_model=schemas.YoutubeVideoOut,
 )
 async def get_youtube_video_insights(youtube_video_in: schemas.YoutubeVideoIn):
-    # TODO: Check if already requested: return answer, or wait for 5 sec.
-    # lock and
-    # Start pipeline with video_id, return finally.
+    logger.info(f'Fetching data for video: {youtube_video_in.video_id}')
+    # TODO: consists of blocked code.
+    # TODO: it could be parallelized, but check above (min time-cons solution: to separate rpc services).
+    youtube_client = YoutubeScrapperClient()
+    video_data = await youtube_client.get_video_data(youtube_video_in.video_id)
 
-    # TODO
-    return schemas.YoutubeVideoOut.construct()
+    logger.info(f'Get model inferences for video: {youtube_video_in.video_id}')
+
+    # await ai_video_insight_client.get_insights()
+    return schemas.YoutubeVideoOut.construct(
+        clickbait_ratio="",
+        video_summary="",
+        comments_summary="",
+    )
