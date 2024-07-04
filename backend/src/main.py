1f03bdb47e70a2f0c93d17d9dc559d6c6091292b
AlcibiadesCleinias <ivan.cheprasov@phystech.edu>
2024-07-04 17:31:41 +0200

    feature/connect-extention-with-back

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,5 +1,6 @@
 from logging.config import dictConfig
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from api.api import api_router
 from config.logger import get_app_logger
@@ -16,3 +17,10 @@ app = FastAPI(
 )
 
 app.include_router(api_router, prefix=settings.API_V1)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
